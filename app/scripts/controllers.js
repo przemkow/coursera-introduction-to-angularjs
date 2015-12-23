622ff04db2b85a5acfc97cd72f8607a86cf8526a
Przemysław Fałowski <przemkow92@gmail.com>
2015-12-23 21:58:55 +0000

    add ng-route, configure SPA

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -64,9 +64,9 @@ angular.module('confusionApp')
   };
 }])
 
-.controller('DishDetailController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+.controller('DishDetailController', ['$scope', '$routeParams', 'menuFactory', function($scope, $routeParams, menuFactory) {
 
-  var dish= menuFactory.getDish(3);
+  var dish= menuFactory.getDish(parseInt($routeParams.id,10));
 
   $scope.dish = dish;
 
